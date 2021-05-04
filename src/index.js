99fb629820e57ac0b6b9239c01b4e067cc248215
Gayatri Chitturi <43856163+gayatrichitturi@users.noreply.github.com>
2021-05-04 11:09:58 +0200

    tried random color

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+var randomColorRGB = require("random-color-rgb");
 
 class Emoji extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emoji: "Color",
+    };
+  }
   render() {
     return (
       <>
-        <h1>Hello</h1>
+        <h1>{this.state.emoji}</h1>
+        <input
+          type="text"
+          value={this.state.emoji}
+          Place
+          holder="Name"
+          onClick={(e) => this.setState({ search: e.target.value })}
+        ></input>
       </>
     );
   }
