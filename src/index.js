2863a7a45aa06034b234ab358f1efd5c3ad6babe
Gayatri Chitturi <43856163+gayatrichitturi@users.noreply.github.com>
2021-05-04 10:42:19 +0200

    emoji

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Emoji extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hello</h1>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<Emoji />, document.getElementById("root"));
